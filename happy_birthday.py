3834896b889ff6431b99a04ea0cf2f2806821919
Gautam Binu <75609533+GautamBinu@users.noreply.github.com>
2025-09-09 09:51:09 +0400

    first one

---
+++ happy_birthday.py
@@ -0,0 +1,50 @@
+"""
+write a program/function that prompts the user once for each of the following: name, birthmonth, birth day,
+birth year and print everything in a sentence
+"""
+
+print("Hello World!")
+
+
+'''
+def hi():
+    # this is a simple function that prints hello in front of the name
+    print("hello " + input("enter your name:- "))
+
+hi()
+'''
+
+'''
+def box():
+    # this is a simple function that prints a box
+    print("*****\n*   *\n*****")
+
+box()
+'''
+
+
+'''
+
+def happy_birthday():
+    """
+    This function
+    prompts the user once for each of the following: name, birthmonth, birth day,
+    birth year and print everything in a sentence
+    """
+    input_name = input("What is your name? :")
+    birthmonth = input("What is your birth month? :")
+    birthday = input("What is your birth day? :")
+    birthyear = input("What is your birth year :")
+    print(input_name + "'s birthday is on "+ birthday + " and their birth month is on " + birthmonth + " and their birth year is on " + birthyear)
+
+happy_birthday()
+'''
+
+def days_of_week():
+    print("Monday, Tuesday")
+days_of_week()
+days_of_week()
+days_of_week()
+
+
+
---
+++ variables.py
@@ -0,0 +1,7 @@
+def variable_practice():
+    age = float(input("enter your age:- "))*12
+    days_in_year = 365
+    name_of_first_pet = input("enter your pets name")
+    pi = 3.14159
+    print(str(age) + " months old", str(days_in_year) + " days in a years", name_of_first_pet + " is the name of your first pet", str(pi) + " is the first 6 digits of pi")
+variable_practice()
