82bb670b9c08bb5a7ec3b63268ff2d5c2a414a2b
Gautam Binu <75609533+GautamBinu@users.noreply.github.com>
2025-09-09 09:53:21 +0400

    variables

--- variables.py
+++ variables.py
@@ -3,5 +3,5 @@ def variable_practice():
     days_in_year = 365
     name_of_first_pet = input("enter your pets name")
     pi = 3.14159
-    print(str(age) + " months old", str(days_in_year) + " days in a years", name_of_first_pet + " is the name of your first pet", str(pi) + " is the first 6 digits of pi")
+    print(str(age) + " months old", str(days_in_year) + " days in a year", name_of_first_pet + " is the name of your first pet", str(pi) + " is the first 6 digits of pi")
 variable_practice()
